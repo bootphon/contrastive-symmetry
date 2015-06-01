2fa6a3d92d565b8d6add59b4b3172023406b8786
Ewan Dunbar <emd@umd.edu>
2015-06-01 02:36:37 +0200

    fixed

--- collapse_minimal.sh
+++ collapse_minimal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd results
+cd results/version_2
 for f in cons_random_feature cons_random_matrix cons_random_segment cons inv stop vowel inv_random_feature inv_random_matrix inv_random_segment stop_random_feature stop_random_segment stop_random_matrix vowel_random_feature vowel_random_matrix vowel_random_segment
 do
-  cat $f\_subminimal/*.csv | sed -e '2,$ s/^language.*//;/^$/d' > $f\_minimal.csv
+  cat $f\_subminimal/*.csv | sed -e '2,$ s/^language.*//;/^$/d' > ../$f\_minimal.csv
 done
 cd ..
