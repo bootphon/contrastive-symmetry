b6966e0d3118c744045c1ae4d8fa4a0bf7bfbcb2
Ewan Dunbar <emd@umd.edu>
2015-08-21 16:53:46 +0200

    moved some things around into a dedicated scripts/ folder, created some scripts to simplify the process of running stats, updated Md

---
+++ scripts/minfeat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+Rscript --vanilla scripts/minfeat.R specs/$1\_specs.csv stats/$1\_minfeat.csv
+
---
+++ scripts/size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+Rscript --vanilla scripts/size.R data/$1\.csv stats/$1\_size.csv
+
