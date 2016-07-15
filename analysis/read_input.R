a306325ded53d076d5a4f0b8b1d0635a4d1a178c
Ewan Dunbar <emd@umd.edu>
2016-07-15 20:28:04 +0200

    cleanup - step one

--- analysis/read_input.R
+++
@@ -1,4 +0,0 @@
-size <- combine_dlist(read_from_filename_table(SIZE_FILENAME_TABLE))
-sum_fbalance_spec <- combine_dlist(read_from_filename_table(SUM_FBALANCE_FILENAME_TABLE))
-sum_fnpairs_spec <- combine_dlist(read_from_filename_table(SUM_FNPAIRS_FILENAME_TABLE))
-extra_geometries <- read.csv(EXTRA_GEOMETRIES)
